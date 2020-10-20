af298e21a577b35d2b255376e4964b2a1fc6ab14
Hulkitos <hulkrojo2@gmail.com>
2020-10-20 16:23:26 +0200

    Update Main.java

--- src/es/hulk/clase/Main.java
+++ src/es/hulk/clase/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Person person = new Person("German", 10, "56431732a", "Masculi", 657654798);
         Person person2 = new Person("Alejandro", 12, "785798a", "Dona", 288576);
+        
         System.out.println(person.getName());
         System.out.println(person.getAge());
         System.out.println(person.getDNI());
