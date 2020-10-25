ed3bd42b37ed584b97c1220ded3bdedb3d652537
Miquel Angel <hulkrojo2@gmail.com>
2020-10-25 13:03:27 +0100

    Added dog object

--- src/es/hulk/clase/Main.java
+++ src/es/hulk/clase/Main.java
@@ -1,16 +1,15 @@
 package es.hulk.clase;
 
+import es.hulk.clase.objects.Dog;
 import es.hulk.clase.objects.Person;
 
 public class Main {
 
     public static void main(String[] args) {
         Person person = new Person("German", 10, "56431732a", "Masculi", 657654798);
-        Person person2 = new Person("Alejandro", 12, "785798a", "Dona", 288576);
-        System.out.println(person.getName());
-        System.out.println(person.getAge());
-        System.out.println(person.getDNI());
-        System.out.println(person.getGenre());
-        System.out.println(person.getTelephone());
+        Dog dog = new Dog("ca", 10, "ca sord", 10, "si");
+
+        dog.display();
+        dog.novaVacuna(true);
     }
 }
