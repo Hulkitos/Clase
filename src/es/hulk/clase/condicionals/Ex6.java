102c8d7f6216df32f7c1bd827c7f50f376c17635
Miquel Angel <hulkrojo2@gmail.com>
2020-10-26 10:05:20 +0100

    Addded some exericises

---
+++ src/es/hulk/clase/condicionals/Ex6.java
@@ -0,0 +1,23 @@
+package es.hulk.clase.condicionals;
+
+import java.util.Scanner;
+
+public class Ex6 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int nota = scanner.nextInt();
+
+        if (nota == 0 || nota == 1 || nota == 2 || nota == 3 || nota == 4 ) {
+            System.out.println("Insuficient");
+        } else if (nota == 5) {
+            System.out.println("Suficient");
+        } else if (nota == 6) {
+            System.out.println("Be");
+        } else if (nota == 7 || nota == 8) {
+            System.out.println("Notable");
+        } else if (nota == 9 || nota == 10) {
+            System.out.println("Excelent");
+        }
+    }
+}
---
+++ src/es/hulk/clase/condicionals/Ex7.java
@@ -0,0 +1,24 @@
+package es.hulk.clase.condicionals;
+
+import java.util.Scanner;
+
+public class Ex7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Dia: ");
+        int dia = scanner.nextInt();
+
+        System.out.print("Mes: ");
+        int mes = scanner.nextInt();
+
+        System.out.print("Any: ");
+        int any = scanner.nextInt();
+
+        if (dia <= 30 || mes <= 12 || any <= 2060) {
+            System.out.println("La data es correcte");
+        } else {
+            System.out.println("La data no es correcte");
+        }
+    }
+}
