2e97417c3e2d19ae76295388ec476e559ce174a4
Miquel Angel <hulkrojo2@gmail.com>
2020-11-03 11:37:02 +0100

    Added Arrays Exercises

--- Programacio/src/es/hulk/clase/Main.java
+++ Programacio/src/es/hulk/clase/Main.java
@@ -1,9 +1,9 @@
 package es.hulk.clase;
 
-import es.hulk.clase.arrays.Ex9;
+import es.hulk.clase.arrays.Ex10;
 
 public class Main {
     public static void main(String[] args) {
-        Ex9.Exercise();
+        Ex10.Exercise();
     }
 }
