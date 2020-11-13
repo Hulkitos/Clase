1858ef647b32d1d8d0bb8cbf6c37d01ddedc85d0
Miquel Angel <hulkrojo2@gmail.com>
2020-11-13 09:36:20 +0100

    Done all arrays and loop exercises

---
+++ Programacio/src/es/hulk/clase/objects/Alumne.java
@@ -0,0 +1,38 @@
+package es.hulk.clase.objects;
+
+public class Alumne {
+
+    private String name;
+    private int id;
+    private int[] notes;
+
+    public Alumne(String name, int id, int[] notes) {
+        this.id = id;
+        this.name = name;
+        this.notes = notes;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNotes(int[] notes) {
+        this.notes = notes;
+    }
+
+    public String getName(String name) {
+        return this.name;
+    }
+
+    public int getId(int id) {
+        return this.id;
+    }
+
+    public int[] getNotes(int[] notes) {
+        return this.notes;
+    }
+}
---
+++ Programacio/src/es/hulk/clase/objects/Assignatura.java
@@ -0,0 +1,28 @@
+package es.hulk.clase.objects;
+
+public class Assignatura {
+
+    private String name;
+    private String[] students;
+
+    public Assignatura(String name, String[] students) {
+        this.name = name;
+        this.students = students;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStudents(String[] students) {
+        this.students = students;
+    }
+
+    public String getName(String name) {
+        return this.name;
+    }
+
+    public String[] getStudents(String[] notes) {
+        return this.students;
+    }
+}
