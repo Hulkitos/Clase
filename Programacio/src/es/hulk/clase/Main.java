39b6070020edd04e1afef96483f548bfd219705a
MIquel Angel <hulkrojo2@gmail.com>
2021-01-07 19:27:27 +0100

    jajaxdxd

--- Programacio/src/es/hulk/clase/Main.java
+++ Programacio/src/es/hulk/clase/Main.java
@@ -1,13 +1,8 @@
 package es.hulk.clase;
 
-import es.hulk.clase.exercisis.Strings;
-import es.hulk.clase.exercisisClasse.Empleat;
-import es.hulk.clase.practica.Alumne;
-import es.hulk.clase.practica.Professor;
-
 public class Main {
 
     public static void main(String[] args) {
-        Strings.ex3("Hi havia una vegada una persona que no sabia com se feia una array");
+        Test.Ex2(new int[]{1, 2, 2, 3, 6});
     }
 }
