ad48053c6416d1cb369437092370e3a6482ffe01
Miquel Angel <hulkrojo2@gmail.com>
2021-02-23 11:48:07 +0100

    Added Cocktail Sort Algorithm

---
+++ Programacio-2/src/es/hulk/programacio/exercises/sortAlgorithms/CockTailSort.java
@@ -0,0 +1,13 @@
+package es.hulk.programacio.exercises.sortAlgorithms;
+
+public class CockTailSort {
+
+    public static void applySort(int[] int_array) {
+        for (int i = 0; i < int_array.length; i++) {
+            for (int j = 0; j < int_array.length; j++) {
+
+            }
+        }
+    }
+
+}
