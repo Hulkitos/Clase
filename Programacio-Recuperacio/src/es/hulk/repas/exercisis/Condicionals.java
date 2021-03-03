aa7d6a8ec871ab442d1be77192dc11784d5e3524
Miquel Angel <hulkrojo2@gmail.com>
2021-03-03 13:38:58 +0100

    Added ex 1 of Condicionals class

--- Programacio-Recuperacio/src/es/hulk/repas/exercisis/Condicionals.java
+++ Programacio-Recuperacio/src/es/hulk/repas/exercisis/Condicionals.java
@@ -1,4 +1,18 @@
 package es.hulk.repas.exercisis;
 
+import java.util.Scanner;
+
 public class Condicionals {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void ex1() {
+        int num = scanner.nextInt();
+
+        if (num < 0) {
+            System.out.println("Negatiu");
+        } else {
+            System.out.println("Positiu");
+        }
+    }
 }
