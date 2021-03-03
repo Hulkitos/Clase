39b30bb8741c9f8f3a217bc1e8a60468b7ee6e72
Miquel Angel <hulkrojo2@gmail.com>
2021-03-03 13:21:10 +0100

    Added class & object Dog

---
+++ Programacio-Recuperacio/src/es/hulk/repas/clases/Dog.java
@@ -0,0 +1,22 @@
+package es.hulk.repas.clases;
+
+public class Dog {
+
+    private String nom;
+    private int edat;
+    private String raça;
+    private int nvacunes;
+    private boolean pedigri;
+
+
+    public Dog (String nom, int edat, String raça, int nvacunes, boolean pedigri) {
+        this.nom = nom;
+        this.edat = edat;
+        this.raça = raça;
+        this.nvacunes = nvacunes;
+        this.pedigri = pedigri;
+    }
+
+    
+
+}
