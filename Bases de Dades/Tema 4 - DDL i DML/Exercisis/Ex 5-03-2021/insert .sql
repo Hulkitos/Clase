acab73363621ee8cd7be99d5091fcee5ff63e25f
Miquel Angel <hulkrojo2@gmail.com>
2021-03-17 09:41:48 +0100

    test

--- Bases de Dades/Tema 4 - DDL i DML/Exercisis/Ex 5-03-2021/insert .sql
+++ Bases de Dades/Tema 4 - DDL i DML/Exercisis/Ex 5-03-2021/insert .sql
@@ -3,5 +3,3 @@ use sales;
 insert into client
     (dni, nameCll, sur1Cll, sur2Cll, phoneNum, phoneType, idCity) values
     ('11111111A', 'Wendoline', 'Sun',null,99999999,'mobile',1);
-
-create user client;
