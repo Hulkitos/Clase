d21461093872803c16e6f1b0a41a78f4226c9a2d
Miquel Angel <hulkrojo2@gmail.com>
2021-04-12 09:52:26 +0200

    Half fixed AppearChars method

--- Repas Programacio/src/es/hulk/repas/practica/AppearChars.java
+++ Repas Programacio/src/es/hulk/repas/practica/AppearChars.java
@@ -2,15 +2,16 @@ package es.hulk.repas.practica;
 
 public class AppearChars extends Practica1 {
 
-    private static char[] array = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-    private static int[] numeros = new int[array.length];
+    private static char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    private static int maxchars = chars.length;
+    private static int[] numeros = new int[chars.length];
 
     public static void CountAppearChars() {
-        int counter = 0;
-        for (int i = 0; i <= text.length(); i++) {
-            for (int j = 0; i < array.length; j++) {
-                for (int k = 0; k < numeros.length; k++) {
-                    System.out.println("Aparicions de Caracter: " + array[j] + " amb un total de " + numeros[k] + " vegades");
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j < maxchars; j++) {
+                if (text.charAt(i) == chars[j]) {
+                    numeros[j]++;
+                    System.out.println("Caracter: " + chars[j] + " | Aparicions: " + numeros[j]);
                 }
             }
         }
