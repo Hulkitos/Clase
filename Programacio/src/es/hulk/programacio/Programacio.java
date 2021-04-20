82fedf81974d4896ba9c7242162d59107d371932
Miquel Angel <hulkrojo2@gmail.com>
2021-04-20 10:13:20 +0200

    fixed last exs

--- Programacio/src/es/hulk/programacio/Programacio.java
+++ Programacio/src/es/hulk/programacio/Programacio.java
@@ -1,15 +1,10 @@
 package es.hulk.programacio;
 
-import es.hulk.programacio.clases.Dog;
 import es.hulk.programacio.exercises.repas.Revisio1;
 
 public class Programacio {
 
     public static void main(String[] args) {
-
-        Revisio1.askFourNumbers();
-
-        Dog ca = new Dog("Calvo", 10, "hotdog", 1, "si");
-        ca.display();
+        Revisio1.calcularFactorial();
     }
 }
