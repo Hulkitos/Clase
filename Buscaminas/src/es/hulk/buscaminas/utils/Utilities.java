73324fb450cd98d9f99f5e5c6be2e3b551a004e3
Miquel Angel <hulkrojo2@gmail.com>
2021-04-26 09:35:16 +0200

    Added Utilities class

---
+++ Buscaminas/src/es/hulk/buscaminas/utils/Utilities.java
@@ -0,0 +1,15 @@
+package es.hulk.buscaminas.utils;
+
+import java.io.Console;
+
+public class Utilities {
+
+    public static void logNewLine(String s) {
+        System.out.println(s);
+    }
+
+    public static void log(String s) {
+        System.out.print(s);
+    }
+
+}
