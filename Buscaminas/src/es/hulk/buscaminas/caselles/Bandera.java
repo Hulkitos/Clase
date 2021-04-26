77dd536becbab0477abe5f06192c563c777aa1c4
Miquel Angel <hulkrojo2@gmail.com>
2021-04-26 12:05:48 +0200

    Need fix logic for Bandera.java

--- Buscaminas/src/es/hulk/buscaminas/caselles/Bandera.java
+++ Buscaminas/src/es/hulk/buscaminas/caselles/Bandera.java
@@ -1,15 +1,27 @@
 package es.hulk.buscaminas.caselles;
 
-import es.hulk.buscaminas.tauler.Tauler;
+import es.hulk.buscaminas.utils.Utilities;
 
-public class Bandera extends Caselles {
+import java.util.Scanner;
 
-    public Bandera(boolean isTapada, boolean isBandera, int aroundBomb, boolean isBomb) {
-        super(isTapada, isBandera, aroundBomb, isBomb);
-    }
+import static es.hulk.buscaminas.tauler.Tauler.getTauler;
 
-    public void insertBanders() {
-        Tauler.getTauler();
+public class Bandera {
 
+    private static Caselles[][] bandera;
+    private static boolean isBandera;
+
+    public static void insertBanders() {
+        Scanner scanner = new Scanner(System.in);
+        bandera = getTauler();
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                Utilities.log(" ( ? ) ");
+                isBandera = true;
+            }
+            Utilities.logNewLine("");
+        }
     }
 }
