9440939cb1e0e8b9f42f53a4067943d05adfbe60
Miquel Angel <hulkrojo2@gmail.com>
2021-04-26 09:42:38 +0200

    Created printTitol method & fixed menu bug

--- Buscaminas/src/es/hulk/buscaminas/menus/Menus.java
+++ Buscaminas/src/es/hulk/buscaminas/menus/Menus.java
@@ -2,11 +2,11 @@ package es.hulk.buscaminas.menus;
 
 import es.hulk.buscaminas.caselles.Caselles;
 import es.hulk.buscaminas.tauler.Tauler;
+import es.hulk.buscaminas.utils.Utilities;
 
 import java.util.Scanner;
 
-import static es.hulk.buscaminas.menus.MenuType.CUSTOM;
-import static es.hulk.buscaminas.menus.MenuType.MAINMENU;
+import static es.hulk.buscaminas.menus.MenuType.*;
 
 public class Menus extends Tauler {
 
@@ -26,21 +26,50 @@ public class Menus extends Tauler {
                 switch (opcio) {
                     case 1:
                         setTauler(new Caselles[8][8]);
+                        printTauler();
+                        type = INGAME;
+                        break;
                     case 2:
                         setTauler(new Caselles[16][16]);
+                        printTauler();
+                        type = INGAME;
+                        break;
                     case 3:
                         setTauler(new Caselles[40][40]);
+                        printTauler();
+                        type = INGAME;
+                        break;
                     case 4:
                         type = CUSTOM;
+                        break;
                 }
+                break;
             case CUSTOM:
-                System.out.println("Custom Tablero");
+                Utilities.logNewLine("");
+                System.out.println("Tablero personalitzat");
+                Utilities.logNewLine("");
+
+                Utilities.log("Trii un tamany per la X: ");
                 int x = scanner.nextInt();
+                Utilities.log("Trii un tamany per la Y: ");
                 int y = scanner.nextInt();
                 setTauler(new Caselles[x][y]);
+                printTauler();
+                break;
             case INGAME:
             case FINSIHED:
         }
+    }
+
+    public static void printTitol() {
+        Utilities.logNewLine("  ____  _    _  _____  _____          __  __ _____ _   _           _____ ");
+        Utilities.logNewLine(" |  _ \\| |  | |/ ____|/ ____|   /\\   |  \\/  |_   _| \\ | |   /\\    / ____|");
+        Utilities.logNewLine(" | |_) | |  | | (___ | |       /  \\  | \\  / | | | |  \\| |  /  \\  | (___  ");
+        Utilities.logNewLine(" |  _ <| |  | |\\___ \\| |      / /\\ \\ | |\\/| | | | | . ` | / /\\ \\  \\___ \\ ");
+        Utilities.logNewLine(" | |_) | |__| |____) | |____ / ____ \\| |  | |_| |_| |\\  |/ ____ \\ ____) |");
+        Utilities.logNewLine(" |____/ \\____/|_____/ \\_____/_/    \\_\\_|  |_|_____|_| \\_/_/    \\_\\_____/ ");
+        Utilities.logNewLine("                                                                         ");
+        Utilities.logNewLine("                                                                         ");
     }
 
     public String getMenu() {
