0353087e69d45dd095063364ec231c0db16ce4ea
Miquel Angel <hulkrojo2@gmail.com>
2021-04-26 12:06:28 +0200

    Fixed redundant code

--- Buscaminas/src/es/hulk/buscaminas/menus/Menus.java
+++ Buscaminas/src/es/hulk/buscaminas/menus/Menus.java
@@ -1,5 +1,6 @@
 package es.hulk.buscaminas.menus;
 
+import es.hulk.buscaminas.caselles.Bandera;
 import es.hulk.buscaminas.caselles.Caselles;
 import es.hulk.buscaminas.tauler.Tauler;
 import es.hulk.buscaminas.utils.Utilities;
@@ -28,44 +29,55 @@ public class Menus extends Tauler {
         if (option == 4) {
             type = CUSTOM;
         }
+        while (option != 5) {
+            switch (type) {
+                case MAINMENU:
+                    switch (option) {
+                        case 1:
+                            setTauler(new Caselles[8][8]);
+                            printTauler();
+                            break;
+                        case 2:
+                            setTauler(new Caselles[16][16]);
+                            printTauler();
+                            break;
+                        case 3:
+                            setTauler(new Caselles[40][40]);
+                            printTauler();
+                            break;
+                        case 5:
+                            exit();
+                            break;
+                    }
+                    break;
+                case CUSTOM:
+                    Utilities.logNewLine("");
+                    System.out.println("Tablero personalitzat");
+                    Utilities.logNewLine("");
 
-        switch (type) {
-            case MAINMENU:
-                switch (option) {
-                    case 1:
-                        setTauler(new Caselles[8][8]);
-                        printTauler();
-                        break;
-                    case 2:
-                        setTauler(new Caselles[16][16]);
-                        printTauler();
-                        break;
-                    case 3:
-                        setTauler(new Caselles[40][40]);
-                        printTauler();
-                        break;
-                }
-                break;
-            case CUSTOM:
-                Utilities.logNewLine("");
-                System.out.println("Tablero personalitzat");
-                Utilities.logNewLine("");
+                    Utilities.log("Trii un numero de files: ");
+                    int x = scanner.nextInt();
+                    Utilities.log("Trii un numero de columnes: ");
+                    int y = scanner.nextInt();
 
-                Utilities.log("Trii un numero de files: "); int x = scanner.nextInt();
-                Utilities.log("Trii un numero de columnes: "); int y = scanner.nextInt();
-
-                setTauler(new Caselles[x][y]);
-                printTauler();
-                break;
-            case INGAME:
-                Utilities.logNewLine("");
-                Utilities.logNewLine("");
-                Utilities.logNewLine("");
-                break;
-            case FINSIHED:
-                Utilities.logNewLine("");
-                Utilities.logNewLine("FINISHED Type");
-                Utilities.logNewLine("");
+                    setTauler(new Caselles[x][y]);
+                    printTauler();
+                    break;
+                case INGAME:
+                    Utilities.logNewLine("");
+                    Utilities.logNewLine("Trii una opcio");
+                    Utilities.logNewLine("");
+                    Utilities.logNewLine("1 - Posar una bandera");
+                    Bandera.insertBanders();
+                    Utilities.logNewLine("2 - Destapa tauler");
+                    break;
+                case FINSIHED:
+                    Utilities.logNewLine("");
+                    Utilities.logNewLine("FINISHED Type");
+                    Utilities.logNewLine("");
+                    printFinish();
+            }
+            display();
         }
     }
 
@@ -84,17 +96,26 @@ public class Menus extends Tauler {
             printTitol();
             Utilities.logNewLine("Trii una de les seguents opcions");
             Utilities.logNewLine("");
-            Utilities.logNewLine("1 - Principiant ");
-            Utilities.logNewLine("2 - Normal");
+            Utilities.logNewLine("1 - Principiant (Tauler 8x8 amb 10 mines");
+            Utilities.logNewLine("2 - Normal (Tauler 16x16 amb 40 mines)");
             Utilities.logNewLine("3 - Dificil");
             Utilities.logNewLine("4 - Personalitzat");
+            Utilities.logNewLine("");
+            Utilities.logNewLine("5 - Surtir del programa");
             Utilities.logNewLine("");
             Utilities.log("Trii una de les opcions: ");
             printMenus(MAINMENU);
         } catch (InputMismatchException exception) {
             display();
         }
+    }
 
+    public static void printFinish() {
+
+    }
+
+    public static void exit() {
+        Utilities.logNewLine("Has sortit del Programa");
     }
 
     public String getMenu() {
