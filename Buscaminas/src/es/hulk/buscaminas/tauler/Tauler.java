3cbec48cb510cb37abed6076693a6acc95aabaac
Miquel Angel <hulkrojo2@gmail.com>
2021-04-29 09:56:20 +0200

    made tauler use object

--- Buscaminas/src/es/hulk/buscaminas/tauler/Tauler.java
+++ Buscaminas/src/es/hulk/buscaminas/tauler/Tauler.java
@@ -5,44 +5,12 @@ import es.hulk.buscaminas.caselles.Caselles;
 import es.hulk.buscaminas.menus.Menu;
 import es.hulk.buscaminas.utils.Utilities;
 
-public class Tauler {
+public class Tauler extends Caselles{
 
     private static Caselles[][] tauler;
-    private static int x;
-    private static int y;
-    private static boolean isFlag;
-    private static boolean isBomb;
 
-    public static int getX() {
-        return x;
-    }
-
-    public static void setX(int x) {
-        Tauler.x = x;
-    }
-
-    public static int getY() {
-        return y;
-    }
-
-    public static void setY(int y) {
-        Tauler.y = y;
-    }
-
-    public static boolean isIsFlag() {
-        return isFlag;
-    }
-
-    public static void setIsFlag(boolean isFlag) {
-        Tauler.isFlag = isFlag;
-    }
-
-    public static boolean isIsBomb() {
-        return isBomb;
-    }
-
-    public static void setIsBomb(boolean isBomb) {
-        Tauler.isBomb = isBomb;
+    public Tauler(boolean isTapada, boolean isBandera, int aroundBomb, boolean isBomb) {
+        super(isTapada, isBandera, aroundBomb, isBomb);
     }
 
     public static Caselles[][] getTauler() {
@@ -57,9 +25,10 @@ public class Tauler {
         if (tauler.length <= 200) {
             for (int i = 0; i < tauler.length; i++) {
                 for (int j = 0; j < tauler[i].length; j++) {
-                    Utilities.log(" [ x ] ");
-                    if (isIsFlag()) {
-                        setTauler(Bandera.getBandera());
+                    tauler[i][j] = new Caselles();
+                    Utilities.log(" [ * ] ");
+                    if (tauler[i][j].isBandera()) {
+                        Bandera.setFlag();
                         Utilities.log(" [ ? ] ");
                     }
                 }
@@ -68,6 +37,5 @@ public class Tauler {
         } else {
             Utilities.logNewLine("Tauler massa gross");
         }
-        Menu.displayInGameMenu();
     }
 }
